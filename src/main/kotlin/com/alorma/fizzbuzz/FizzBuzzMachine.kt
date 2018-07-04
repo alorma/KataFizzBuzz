8214eaf73b2e87afd3dd2a0ea9759a79411495f0
Bernat Borrás Paronella <bernat.borras@privalia.com>
2018-07-04 09:46:15 +0200

    Add Base project

---
+++ src/main/kotlin/com/alorma/fizzbuzz/FizzBuzzMachine.kt
@@ -0,0 +1,4 @@
+package com.alorma.fizzbuzz
+
+class FizzBuzzMachine {
+}
---
+++ src/test/kotlin/com/alorma/fizzbuzz/FizzBuzzMachineTest.kt
@@ -0,0 +1,14 @@
+package com.alorma.fizzbuzz
+
+import org.junit.Before
+
+class FizzBuzzMachineTest {
+
+    private lateinit var machine: FizzBuzzMachine
+
+    @Before
+    fun setup() {
+        machine = FizzBuzzMachine()
+    }
+
+}
